7b1153fb33526ef129a2874530ae9d3a5f7cb0dd
Taylorhc0 <146321796+Taylorhc0@users.noreply.github.com>
2023-09-29 10:39:30 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,22 @@ function updateCountdown() {
     const targetDate = new Date('2024-01-01T00:00:00');
     const currentDate = new Date();
 
-    const totalSeconds = (targetDate - currentDate) / 1000;
+    let totalSeconds = (targetDate - currentDate) / 1000;
+
+    if (totalSeconds <= 0) {
+        totalSeconds = 0;
+    }
 
     const days = Math.floor(totalSeconds / 3600 / 24);
     const hours = Math.floor(totalSeconds / 3600) % 24;
     const minutes = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds % 60);
 
-    document.getElementById('day').textContent = days;
-    document.getElementById('hours').textContent = hours;
-    document.getElementById('minutes').textContent = minutes;
-    document.getElementById('seconds').textContent = seconds;
+    document.getElementById('day').textContent = String(days).padStart(2, '0');
+    document.getElementById('hours').textContent = String(hours).padStart(2, '0');
+    document.getElementById('minutes').textContent = String(minutes).padStart(2, '0');
+    document.getElementById('seconds').textContent = String(seconds).padStart(2, '0');
 }
 
-// Update the countdown every second
 setInterval(updateCountdown, 1000);
 updateCountdown();
